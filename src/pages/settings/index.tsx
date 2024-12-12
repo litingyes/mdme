1d89b87a644cab187c2821a8a2ed57d21b56064d
litingyes <luz.liting@gmail.com>
2024-12-12 20:29:21 +0800

    style(setting): adjust tabs style

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -11,8 +11,8 @@ const MODULES = [
 export default function Settings() {
   return (
     <main className="h-screen w-screen overflow-auto bg-gray-50 p-4 text-neutral-800 transition-colors dark:(bg-gray-900 text-neutral-100)">
-      <TabGroup className="flex gap-4">
-        <TabList>
+      <TabGroup className="h-full flex gap-4">
+        <TabList className="max-h-full overflow-auto rounded-lg bg-gray-200/50 p-2 backdrop-blur-md dark:bg-gray-700/50">
           {
             MODULES.map(item => <Tab key={item.label} className="cursor-pointer rounded-lg px-2 py-1 text-sm font-semibold transition-colors data-[hover]:(bg-gray-200) !data-[active]:(bg-blue-600 text-white) !data-[selected]:(bg-blue-500 text-white) dark:!data-[active]:bg-blue-600">{item.label}</Tab>)
           }
