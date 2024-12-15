25badf8e5c2986aad0d3847364f5c4513323103d
litingyes <luz.liting@gmail.com>
2024-12-15 15:58:59 +0800

    fix(symbol): repair inherit node name

--- src/components/editor/extensions/symbol/index.ts
+++ src/components/editor/extensions/symbol/index.ts
@@ -1,5 +1,7 @@
+import type { Transaction } from '@tiptap/pm/state'
 import type { GlobalAttributes } from '@tiptap/react'
-import { Extension } from '@tiptap/react'
+import { Plugin, PluginKey } from '@tiptap/pm/state'
+import { combineTransactionSteps, Extension, findChildrenInRange, getChangedRanges } from '@tiptap/react'
 
 export const symbol = Extension.create({
   name: 'symbol',
@@ -24,5 +26,31 @@ export const symbol = Extension.create({
     })) as GlobalAttributes))
 
     return attrs
+  },
+  addProseMirrorPlugins() {
+    const key = new PluginKey(this.name)
+
+    return [
+      new Plugin({
+        key,
+        appendTransaction(trs, { doc: oldDoc }, { doc: newDoc, tr }) {
+          if (!trs.find(tr => tr.docChanged) || newDoc.eq(oldDoc)) {
+            return
+          }
+
+          const transform = combineTransactionSteps(oldDoc, trs as Transaction[])
+          getChangedRanges(transform).forEach(({ newRange }) => {
+            const newNodes = findChildrenInRange(newDoc, newRange, node => !node.isText)
+
+            newNodes.forEach(({ node, pos }) => {
+              // fix error inherit
+              tr.setNodeAttribute(pos, 'nodeName', node.type.spec.attrs?.nodeName?.default)
+            })
+          })
+
+          return tr
+        },
+      }),
+    ]
   },
 })
